eb763d18f1190c151458f81b23a3b20c4050aae5
athulyashyju <athulya.shyju@gmail.com>
2019-01-17 23:15:28 +0530

    Add files via upload

---
+++ user.c
@@ -0,0 +1,465 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+int n,w,v,q=1,g,i,z,r1,t,flightno,no,hotelno,roomno,ch7,ch3;
+int plane[10] = {0},ny, p=0,irstClass=1,economy=6,choice;
+char response[2],firstname[35], lastname[35],dept[10],arr[10],fname[10],lname[10],k[10],l[10],f[10],
+age[10],dptdate[20],tt[10],rtndate[10],hotel[10];
+void displayair()
+{
+  printf("**********************************\n");
+  printf("PACKAGE:\t");
+  if(ch==1)
+  {
+    printf("ASIA HOLIDAY PACKAGE\n");
+  }else
+  {
+    printf("MAURITIUS TOUR PACKAGE\n");
+  }
+  if(ch3==1)
+  {
+    printf("EUROPE TOUR PACKAGE\n");
+  }else
+  {
+    printf("MANALI TOUR PACKAGE\n");
+  }
+  if(choice==1)
+  {
+    printf("Flight Type: \tFirstClass\n");
+  }
+  else
+  {
+    printf("Flight Type: \tEconomy\n");
+  }
+  printf("\nDEPARTURE AIRPORT:\t%s\n",dept);
+printf("\nARRIVAL AIRPORT:\t%s\n",arr);
+printf("DEPARTURE DATE:\t%s\n",dptdate);
+if(tt=="2")
+{
+printf("RETURNING DATE:\t%s\n",rtndate);
+}
+printf("No of Passengers:\t%d\n",ny);
+printf("Selected Flight No:\t%d \n",flightno);
+}
+void airbook()
+{
+  printf("********************************************************\n");
+  printf("\t\t TRIP\n");
+  printf("********************************************************\n");
+  printf("AVAILABLE PACKAGES\n");
+  printf("1.FAMILY PACKAGES\n");
+  printf("2.GROUP PACKAGES\n");
+  printf("Select Choice \n");
+  scanf("%d",&c);
+  printf("*****************************************************\n");
+  if(c==1)
+    {printf("*****************************************************\n");
+      printf("1.ASIA HOLIDAY PACKAGE\n");
+        printf("2.MAURITIUS TOUR PACKAGE\n");
+        printf("Select the choice\n");
+        scanf("%d",&ch);
+    }
+  else
+  {
+    printf("*****************************************************\n");
+  printf("1.EUROPE TOUR PACKAGE\n");
+        printf("2.MANALI TOUR PACKAGE\n");
+        printf("Select the choice\n");
+        scanf("%d",&ch3);
+        
+  }
+        
+  
+printf("\nENTER THE DEPARTURE AIRPORT:");
+scanf("%s",dept);
+printf("\nENTER THE ARRIVAL AIRPORT:");
+scanf("%s",arr);
+printf("DEPARTURE DATE:\n");
+scanf("%s",dptdate);
+printf("Type '1' for ONE WAY \n '2' for ROUND TRIP:\n");
+scanf("%s",tt);
+if(tt=="2")
+{
+printf("RETURNING DATE:\n");
+scanf("%s",rtndate);
+}
+printf("*****************************************************\n");
+printf("Which Flight do you prefer?\n");
+printf("1.AIRINDIA\n");
+printf("2.JETAIRWAYS\n");
+printf("3.AIRARABIA\n");
+printf("4.AIRASIA\n");
+printf("5.JETLITE\n");
+printf("*****************************************************\n");
+printf("Select the No :\n");
+scanf("%d",&no);
+switch(no)
+{
+  case  1 : printf("Flights Available for AIRINDIA:\n");
+          airline();
+          printf("Select Flight No:\n");
+          scanf("%d",&flightno);
+          break;
+case  2 : printf("Flights Available for JETAIRWAYS:\n");
+        airline();
+        printf("Select Flight No:\n");
+        scanf("%d",&flightno);
+        break;
+case  3 : printf("Flights Available for AIRARABIA:\n");
+        airline();
+        printf("Select Flight No:\n");
+        scanf("%d",&flightno);
+        break;
+case  4 : printf("Flights Available for AIRASIA:\n");
+        airline();
+        printf("Select Flight No:\n");
+        scanf("%d",&flightno);
+        break;
+case  5 : printf("Flights Available for JETLITE:\n");
+        printf("FlightNo \t From-To \t\t Date \t Time \t Fair\n");
+        airline();
+        printf("Select Flight No:\n");
+        scanf("%d",&flightno);
+        break;
+
+
+}
+
+printf("No of Passengers:\n");
+scanf("%d",&ny);
+
+    while(p<ny)
+{
+  
+  printf("\n%s\n%s\n", "Please type 1 for \"First class\"","Please type 2 for\"Economy\"");
+  scanf("%d", &choice);
+  printf("Do you want side seat ? Yes/No?\n");
+  scanf("%s",f);
+  if(choice==1)
+  {
+    p=p+1;
+
+    printf("\nEnter first name:");
+    scanf("%s",firstname);
+
+    printf("\nEnter lastname :");
+    scanf("%s", lastname);
+
+    printf(" ****************************************\n");
+    printf(" *                            *\n");
+    printf("    NAME:%s, %s                 \n",lastname,firstname);
+    printf(" *                            *\n");
+    printf("    CLASS:%d                      \n",choice);
+    printf(" *                            *\n");
+    printf("    SEAT:%d                      \n",p);
+    printf(" *                            *\n");
+    printf("                               \n");
+    printf(" ****************************************\n");
+}
+
+else if(choice==2)
+{
+
+  
+    p=p+1;
+   
+
+    printf("\nEnter first name:");
+    scanf("%s",firstname);
+
+    printf("\nEnter lastname :");
+    scanf("%s", lastname);
+
+    printf(" ****************************************\n");
+    printf(" *                            *\n");
+    printf("    NAME:%s, %s                 \n",lastname,firstname);
+    printf(" *                            *\n");
+    printf("    CLASS:%d                      \n",choice);
+    printf(" *                            *\n");
+    printf("    SEAT:%d                      \n",p);
+    printf(" *                            *\n");
+    printf("                               \n");
+    printf(" ****************************************\n");
+
+}
+if(p>ny) 
+{
+  printf("seat is full\n");
+}
+}
+
+
+void foode()
+{
+printf("Food and Beverages\n");
+printf("Non-veg or Veg?\n");
+scanf("%s",k);
+printf("Do you want Beverages facilities? Yes or No?\n");
+scanf("%s",l);
+}
+void lugg()
+{
+  printf("Allowed luggage =20kg\n");
+printf("Want to upgrade luggage? 1=Yes or 2=No\n");
+scanf("%d",&v);
+if(v==1)
+{
+  printf("500 per kg\n");
+}
+else
+{
+  break;
+}
+}
+
+void airtickt()
+{
+  printf("*******************************************\n");
+  printf("\tBook Ticket -1\n");
+  printf("\tPrint Ticket Details-2\n");
+  switch(ch7)
+  {
+    case  1 :  airbook();
+                break;    
+    case  2 :   lugg();
+                break;
+    case  3 :   foode();
+                break;
+    case  4 :  displayair();
+        break;
+     default:
+             printf("Not a valid input at this stage\n");
+}
+}
+void cab()
+{
+ printf("*******************************************\n");
+  printf("\tBook Cab-1\n");
+  printf("\tPrint Details-2\n");
+  switch(ch7)
+  {
+    case  1 :  cabbook();
+                break;
+    case  2 :  displaycab();
+                break;
+     default:
+             printf("Not a valid input at this stage\n");
+}
+}
+void displaycab()
+{
+  if(w==1)
+  {
+    printf("*******************************************\n");
+    printf("Vehicle:Two Wheeler\n");
+    printf("*******************************************\n");
+    if (r==1)
+    {
+      printf("Model:CBR\tPrice: 9200\n");
+    }
+    else if (r==2)
+    {
+      printf("Model:Vespa\tPrice: 12322\n");
+    }
+    else if (r==3)
+    {
+      printf("Model:Harley Davidson\tPrice: 21322\n");
+    }
+    else
+         printf("Model:Kawasaki\tPrice:12000\n");
+    }
+  }
+  else if(w==3)
+  {
+    printf("*******************************************\n");
+    printf("Vehicle:Four-Wheeler\n");
+    printf("*******************************************\n");
+    if (r1==1)
+    {
+      printf("Model:BMW\tPrice: 129200\n");
+    }
+    else if (r1==2)
+    {
+      printf("Model:Audi\tPrice: 2212322\n");
+    }
+    else if (r1==3)
+    {
+      printf("Model:Bugatti\tPrice: 2221322\n");
+    }
+    else
+    {
+         printf("Model:Ferrari\tPrice:4312000\n");
+    }
+   }
+  
+}
+void cabbook()
+{
+  printf("*****************************************************\n");
+printf("Select the option\n");
+printf("1.Two Wheeler\n");
+printf("2.Three Wheeler\n");
+printf("3.Four Wheeler\n");
+scanf("%d",&w);
+printf("*****************************************************\n");
+switch(w)
+{
+  case 1  : printf("Two-Wheelers");
+        printf("Available Models \t\t Price per day\n");
+        printf("1.CBR \t\t\t 9200\n");
+        printf("2.Vespa \t\t\t 12322\n");
+        printf("3.Harley Davidson\t\t 21322\n");
+        printf("4.Kawasaki \t\t\t 12000\n");
+        printf("Select Model\n");
+        scanf("%d",&r);
+        printf("No of Passengers:\n");
+        scanf("%d",&g);
+        if(g>2)
+        {
+          goto a;
+        }       
+        break;
+  case 2  : printf("Three-Wheelers");
+        printf("Unavailable");
+        break;
+  case 3  : 
+        a : printf("Four-Wheelers");
+        printf("Available Models \t\t Price per day\n");
+        printf("1.BMW \t\t\t 129200\n");
+        printf("2.Audi \t\t\t 2212322\n");
+        printf("3.Bugatti\t\t\t 2221322\n");
+        printf("4.Ferrari \t\t\t 4312000\n");
+        printf("Select Model\n");
+        scanf("%d",&r1);
+        break;
+}
+}
+void hotelww()
+{
+  printf("*****************************************************\n");
+  printf("\tBook Room-1\n");
+  printf("\tPrint Details-2\n");
+  switch(ch7)
+  {
+    case  1 :  bookhotel();
+                break;
+    case  2 :  displayhotel();
+                break;
+     default:
+             printf("Not a valid input at this stage\n");
+}
+}
+void bookhotel()
+{
+  printf("Available Hotel rooms near %s\n",arr);
+  printf("Hotel Name \t Place \t Room Fair\n");
+  printf("1.Taj \t\t %s \t 1098\n",arr);
+  printf("2.Zisx \t\t %s \t 2198\n",arr);
+  printf("3.Flog \t\t %s \t 1298\n",arr);
+  printf("5.Santorini \t %s \t 1098\n",arr);
+  printf("4.Dave \t\t%s \t 1598\n",arr);
+  printf("5.Vere \t \t%s \t 1498\n",arr);
+  printf("Select the Hotel No.:\n");
+  scanf("%d",&hotelno);
+  printf("*****************************************************\n");
+switch(hotelno)
+{
+  case  1 : printf("Rooms available for Taj Hotel\n");
+          highhotel();
+          printf("Select Room Type No:\n");
+        scanf("%d",&roomno);
+          break;
+  case  2 : printf("Rooms available for Zisx Hotel\n");
+          normalhotel();
+          printf("Select Room Type No:\n");
+        scanf("%d",&roomno);
+          break;
+  case  3 : printf("Rooms available for Flog Hotel\n");
+          highhotel();
+          printf("Select Room Type No:\n");
+        scanf("%d",&roomno);
+          break;
+  case  4 : printf("Rooms available for Santorini Hotel\n");
+          normalhotel();
+          printf("Select Room Type No:\n");
+        scanf("%d",&roomno);
+          break;
+
+  case  5 : printf("Rooms available for Vere Hotel\n");
+          normalhotel();
+          printf("Select Room Type No:\n");
+        scanf("%d",&roomno);
+          break;
+  }
+
+}
+
+
+void highhotel()
+{
+printf("1.Single\n");
+printf("2.Double\n");
+printf("3,Triple\n");
+printf("4.Quad\n");
+printf("5.Queen\n");
+printf("6.King\n");
+}
+void normalhotel()
+{
+printf("1.Single\n");
+printf("2.Double\n");
+printf("3,Triple\n");
+}
+void airline()
+{
+printf("FlightNo \t From-To \t\t Date \t Time \t Fair\n");
+printf("1531 \t %s-%s \t %s \t 07:12-11:50 \t 14444\n",dept,arr,dptdate);
+printf("1235 \t %s-%s \t %s \t 22:24-01:00 \t 17000\n",dept,arr,dptdate);
+printf("21335 \t %s-%s \t %s \t 22:24-01:00 \t 12100\n",dept,arr,dptdate);
+printf("6236 \t %s-%s \t %s \t 07:12-11:30 \t 1114\n",dept,arr,dptdate);
+printf("92433 \t %s-%s \t %s \t 08:23-12:50 \t 21300\n",dept,arr,dptdate);
+printf("22276 \t %s-%s \t %s \t 16:12-21:30 \t 19644\n",dept,arr,dptdate);
+}
+int c,ch,d,r,ch2;
+
+void main()
+{
+ 
+    clrscr();
+    while(q>0)
+{
+  printf("************************************************************\n");
+  printf("\t \t MENU\n");
+    printf("\n\t\t\t\tAirticket Booking - 1\n\t\t\t\tCab Booking - 2\n\t\t\t\tHotel Booking Ticket - 3\n\t\t\t\tExit - 4\n\n\t\t\t\tEnter:");
+    scanf("%d",&ch2);
+            switch(ch2)
+            {
+                case 1:
+                    airtickt();
+                    break;
+                case 2:
+                    cab();
+                    break;
+                case 3:
+                    hotelww();              
+                    break;
+                case 4:
+                      exit(0);
+                default:
+                    printf("Not a valid input at this stage\n");
+            }
+    
+
+
+
+
+}
+
+printf("\nDo you want to continue? 1=Yes or 0=No");
+scanf("%d",&q);
+if(q==0)
+{
+  exit(0);
+}
+}
+}
